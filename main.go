f70f5bfdb26ab1eaff715a6d02bb3888ddc1495f
Kafaa Billahi Syahida <kafaa.developer@gmail.com>
2026-01-22 11:37:04 +0700

    feat: add check health endpoint

--- main.go
+++ main.go
@@ -1,12 +1,24 @@
 package main
 
 import (
+	"encoding/json"
 	"log"
 	"net/http"
 )
 
 func main() {
+	http.HandleFunc("GET /health", checkHealthHandler)
+
 	log.Println("Server listening on port 8080")
 
 	log.Fatal(http.ListenAndServe(":8080", nil))
+}
+
+func checkHealthHandler(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+
+	json.NewEncoder(w).Encode(map[string]string{
+		"status":  "OK",
+		"message": "API Running",
+	})
 }
