ef40a31c9a067f8292575c313038529dfd0e3f16
Kafaa Billahi Syahida <kafaa.developer@gmail.com>
2026-01-22 14:13:20 +0700

    feat: add delete product by ID endpoint

--- main.go
+++ main.go
@@ -27,6 +27,7 @@ func main() {
 	http.HandleFunc("POST /api/products", createProductHandler)
 	http.HandleFunc("GET /api/products/{id}", getProductHandler)
 	http.HandleFunc("PUT /api/products/{id}", updateProductHandler)
+	http.HandleFunc("DELETE /api/products/{id}", deleteProductHandler)
 
 	log.Println("Server listening on port 8080")
 
@@ -109,6 +110,31 @@ func updateProductHandler(w http.ResponseWriter, r *http.Request) {
 			w.Header().Set("Content-Type", "application/json")
 
 			json.NewEncoder(w).Encode(product)
+
+			return
+		}
+	}
+
+	http.Error(w, "Product not found", http.StatusNotFound)
+}
+
+func deleteProductHandler(w http.ResponseWriter, r *http.Request) {
+	idStr := r.PathValue("id")
+	id, err := strconv.Atoi(idStr)
+	if err != nil {
+		http.Error(w, "Invalid Product ID", http.StatusBadRequest)
+		return
+	}
+
+	for i, product := range products {
+		if product.ID == id {
+			products = append(products[:i], products[i+1:]...)
+
+			w.Header().Set("Content-Type", "application/json")
+
+			json.NewEncoder(w).Encode(map[string]string{
+				"message": "sukses delete",
+			})
 
 			return
 		}
