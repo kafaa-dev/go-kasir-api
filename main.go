eac6c993aa323a811efd1cb8911fc6438f53b8e7
Kafaa Billahi Syahida <kafaa.developer@gmail.com>
2026-01-22 11:37:52 +0700

    feat: add list products endpoint
    The products are stored in memory for now.

--- main.go
+++ main.go
@@ -6,8 +6,23 @@ import (
 	"net/http"
 )
 
+type Product struct {
+	ID    int    `json:"id"`
+	Name  string `json:"name"`
+	Price int    `json:"price"`
+	Stock int    `json:"stock"`
+}
+
+var products = []Product{
+	{ID: 1, Name: "Mie rebus", Price: 3500, Stock: 10},
+	{ID: 2, Name: "Air minum 800ml", Price: 3000, Stock: 40},
+	{ID: 3, Name: "Kecap", Price: 12000, Stock: 20},
+}
+
 func main() {
 	http.HandleFunc("GET /health", checkHealthHandler)
+
+	http.HandleFunc("GET /api/products", listProductsHandler)
 
 	log.Println("Server listening on port 8080")
 
@@ -21,4 +36,10 @@ func checkHealthHandler(w http.ResponseWriter, r *http.Request) {
 		"status":  "OK",
 		"message": "API Running",
 	})
+}
+
+func listProductsHandler(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+
+	json.NewEncoder(w).Encode(products)
 }
