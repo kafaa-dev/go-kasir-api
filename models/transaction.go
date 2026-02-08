b01376b9a37207ee63e628c01e990d95dce06be8
Kafaa Billahi Syahida <kafaa.developer@gmail.com>
2026-02-08 21:58:09 +0700

    chore(models): add Transaction and TransactionDetail structs

---
+++ models/transaction.go
@@ -0,0 +1,16 @@
+package models
+
+type Transaction struct {
+	ID          int                 `json:"id"`
+	TotalAmount int                 `json:"total_amount"`
+	Details     []TransactionDetail `json:"details"`
+}
+
+type TransactionDetail struct {
+	ID            int    `json:"id"`
+	TransactionID int    `json:"transaction_id"`
+	ProductID     int    `json:"product_id"`
+	ProductName   string `json:"product_name,omitempty"`
+	Quantity      int    `json:"quantity"`
+	Subtotal      int    `json:"subtotal"`
+}
